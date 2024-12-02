9ae65427f224824a059eb383d821c65cac397d3e
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-12-02 14:15:22 +0900

    test : 매 테스트마다 스케줄 초기화 되도록 수정

--- src/test/java/com/coffee_shop/coffeeshop/service/IntegrationTestSupport.java
+++ src/test/java/com/coffee_shop/coffeeshop/service/IntegrationTestSupport.java
@@ -1,7 +1,9 @@
 package com.coffee_shop.coffeeshop.service;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class IntegrationTestSupport {
 }
