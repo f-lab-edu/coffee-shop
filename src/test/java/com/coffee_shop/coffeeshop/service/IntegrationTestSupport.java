19cac226d6dbf9a44c0a4c155d0bd1dca8c7706c
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-12-04 16:58:00 +0900

    buid : TestContainer 의존성 추가 test : redis TestContainer 적용

--- src/test/java/com/coffee_shop/coffeeshop/service/IntegrationTestSupport.java
+++ src/test/java/com/coffee_shop/coffeeshop/service/IntegrationTestSupport.java
@@ -2,8 +2,28 @@ package com.coffee_shop.coffeeshop.service;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class IntegrationTestSupport {
+	private static final String REDIS_IMAGE = "redis:7.0.8-alpine";
+	private static final int REDIS_PORT = 6379;
+
+	@Container
+	private static GenericContainer<?> redisContainer =
+		new GenericContainer<>(REDIS_IMAGE)
+			.withExposedPorts(REDIS_PORT);
+
+	@DynamicPropertySource
+	private static void registerRedisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", redisContainer::getHost);
+		registry.add("spring.redis.port", () -> redisContainer.getMappedPort(REDIS_PORT)
+			.toString());
+	}
 }
