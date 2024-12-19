2903f7c6855a035d6a2699042d88fea4397d4818
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-12-19 18:38:18 +0900

    feat : redis 최대 재시도 횟수 초과시 실패 이력 저장하도록 수정 test : redis FailHandler 테스트 코드 수정

--- src/main/java/com/coffee_shop/coffeeshop/service/coupon/issue/fail/RedisCouponIssueFailHandler.java
+++ src/main/java/com/coffee_shop/coffeeshop/service/coupon/issue/fail/RedisCouponIssueFailHandler.java
@@ -1,11 +1,21 @@
 package com.coffee_shop.coffeeshop.service.coupon.issue.fail;
 
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.coffee_shop.coffeeshop.common.exception.BusinessException;
+import com.coffee_shop.coffeeshop.domain.coupon.Coupon;
+import com.coffee_shop.coffeeshop.domain.coupon.CouponIssueFailHistory;
 import com.coffee_shop.coffeeshop.domain.coupon.repository.CouponFailCountRepository;
+import com.coffee_shop.coffeeshop.domain.coupon.repository.CouponIssueFailHistoryRepository;
 import com.coffee_shop.coffeeshop.domain.coupon.repository.CouponIssueRepository;
+import com.coffee_shop.coffeeshop.domain.coupon.repository.CouponRepository;
+import com.coffee_shop.coffeeshop.domain.user.User;
+import com.coffee_shop.coffeeshop.domain.user.UserRepository;
+import com.coffee_shop.coffeeshop.exception.ErrorCode;
 import com.coffee_shop.coffeeshop.service.coupon.dto.request.CouponApplication;
 
 import lombok.RequiredArgsConstructor;
@@ -17,16 +27,38 @@ public class RedisCouponIssueFailHandler {
 	private static final int MAX_FAIL_COUNT = 3;
 	private final CouponIssueRepository couponIssueRepository;
 	private final CouponFailCountRepository couponFailCountRepository;
+	private final UserRepository userRepository;
+	private final CouponRepository couponRepository;
+	private final CouponIssueFailHistoryRepository couponIssueFailHistoryRepository;
 
 	public void handleFail(CouponApplication couponApplication, Exception exception) {
 		Long failCount = couponFailCountRepository.increment(couponApplication.getUserId());
 
 		if (failCount >= MAX_FAIL_COUNT) {
 			log.warn("최대 실패 횟수 {}회를 초과하였습니다. 실패 횟수 : {}", MAX_FAIL_COUNT, failCount, exception);
-		} else {
-			log.warn("쿠폰 발급 실패 > 재시도 횟수 : {}, 사용자 ID : {}, 쿠폰 ID : {}", failCount, couponApplication.getUserId(),
-				couponApplication.getCouponId(), exception);
-			couponIssueRepository.add(couponApplication, System.currentTimeMillis());
+
+			Coupon coupon = findCoupon(couponApplication.getCouponId());
+			User user = findUser(couponApplication.getUserId());
+
+			CouponIssueFailHistory history = CouponIssueFailHistory.of(user, coupon,
+				LocalDateTime.now());
+			couponIssueFailHistoryRepository.save(history);
+			return;
 		}
+
+		log.warn("쿠폰 발급 실패 > 재시도 횟수 : {}, 사용자 ID : {}, 쿠폰 ID : {}", failCount, couponApplication.getUserId(),
+			couponApplication.getCouponId(), exception);
+		couponIssueRepository.add(couponApplication, System.currentTimeMillis());
+	}
+
+	private Coupon findCoupon(Long couponId) {
+		return couponRepository.findById(couponId)
+			.orElseThrow(
+				() -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND, "Coupon Not Found, 쿠폰 ID : " + couponId));
+	}
+
+	private User findUser(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND, "User Not Found, 사용자 ID : " + userId));
 	}
 }
