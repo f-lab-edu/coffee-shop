7f4631f580fc28fe63bdb26ae3a3aa050b039542
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-12-23 15:40:49 +0900

    refactor : @Component -> @Service로 어노테이션 변경

--- src/main/java/com/coffee_shop/coffeeshop/service/coupon/issue/fail/RedisCouponIssueFailHandler.java
+++ src/main/java/com/coffee_shop/coffeeshop/service/coupon/issue/fail/RedisCouponIssueFailHandler.java
@@ -2,7 +2,7 @@ package com.coffee_shop.coffeeshop.service.coupon.issue.fail;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.coffee_shop.coffeeshop.domain.coupon.repository.CouponFailCountRepository;
 import com.coffee_shop.coffeeshop.domain.coupon.repository.CouponIssueRepository;
@@ -11,7 +11,7 @@ import com.coffee_shop.coffeeshop.service.coupon.dto.request.CouponApplication;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Component
+@Service
 public class RedisCouponIssueFailHandler {
 	private static final Logger log = LoggerFactory.getLogger(RedisCouponIssueFailHandler.class);
 	private static final int MAX_FAIL_COUNT = 3;
